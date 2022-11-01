01a8ce3b06256e0b01993ad048686606c90b3d12
sunguti-albright <sungutialbright@gmail.com>
2022-11-02 00:16:02 +0300

    ft:add canvas and function to display line graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
 import { Component } from '@angular/core';
+import { ChartService } from './chart.service';
+import { Chart, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'linechart';
+  result: any;
+  companyName: any;
+  closingPrice: number=4;
+  startDate: any;
+  endDate: any;
+  chart: any;
+
+  constructor(private service: ChartService) {
+    Chart.register(...registerables);
+  }
+  ngOnInit() {
+    this.fetchData();
+  }
+  fetchData() {
+    let endpoint = '/FB/data.json?api_key=nQgyfDRtAibDZAV67VSq&limit=15';
+    if (this.endDate && this.startDate){
+      endpoint = endpoint+'&start_date='+this.startDate+'&end_date='+this.endDate
+    }
+    if(this.companyName){
+      endpoint = this.companyName+'/'+endpoint
+    } else {
+      endpoint =  'WIKI/' + endpoint
+    }
+    this.service
+      .nasdaqData(endpoint)
+      .then((res) => {
+        this.result = res;
+        console.log(res);
+        console.log(this.result.dataset_data.data);
+
+        let labels = [];
+        let datasets = [];
+        for (let i = 0; i < this.result.dataset_data.data.length; i++) {
+          labels.push(this.result.dataset_data.data[i][0]);
+          datasets.push(this.result.dataset_data.data[i][this.closingPrice]);
+        }
+
+        this.chart = new Chart('canvas', {
+          type: 'line',
+          data: {
+            labels: labels,
+            datasets: [
+              {
+                label: 'Closing Price',
+                data: datasets,
+                borderWidth: 1,
+                fill: false,
+              },
+            ],
+          },
+        });
+      });
+  }
+
+  selectPrice(event: any) {
+    this.chart.destroy();
+    this.closingPrice=(event.target.value);
+    this.fetchData();
+    console.log(event.target.value);
+  }
+  setStartDate(event: any) {
+    this.startDate = event.target.value;
+    console.log(event.target.value);
+  }
+  setEndDate(event: any) {
+    this.endDate = event.target.value;
+    this.chart.destroy();
+    this.fetchData();
+  }
+  selectCompany(event: any) {
+    this.chart.destroy();
+    this.companyName = event.target.value;
+    this.fetchData()
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ChartService } from './chart.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [ChartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
